f72f060902b704a316361c9b3514f3479a63eca6
Shaurya <shahi.shaurya@gmail.com>
2016-06-20 00:05:02 +0530

    sp

--- app/mailers/employer_mailer.rb
+++ app/mailers/employer_mailer.rb
@@ -1,8 +1,9 @@
 class EmployerMailer < ActionMailer::Base
+  default :from => "shahi.shaurya@gmail.com"
 
   def registration_confirmation(employer)
     @employer = employer
-    mail(to: @employer.email, subject: "Email Verification - BrahmaEmployers")
+    mail(:to => "#{employer.name} <#{employer.email}>", :subject => "Registration Confirmation")
   end
 
 end
